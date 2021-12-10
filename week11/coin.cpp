bdb34de6721b7503e9fddfdd0af7504d2592adba
Shivansh Aggarwal <shivansh.a.8888@gmail.com>
2021-12-10 23:23:31 +0530

    WEEK 11 SOLUTION

---
+++ week11/coin.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(int S[], int m, int n)
+{
+	if (n == 0)
+		return 1;
+
+	if (n < 0)
+		return 0;
+
+	if (m <= 0 && n >= 1)
+		return 0;
+
+	return count(S, m - 1, n) +
+		count(S, m, n - S[m - 1]);
+}
+
+int main()
+{
+	int i, j,n,sum;
+    cin>>n;
+    int arr[n];
+	for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cin>>sum;
+	int m = sizeof(arr) / sizeof(arr[0]);
+	cout << "POSSIBLE OUTCOMES : " << count(arr, m, sum);
+	return 0;
+}
